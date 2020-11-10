89f4666f62ee27b9d2540b0b78cee577aff432f9
Francesco Luzzana <luzzanafrancesco@gmail.com>
2020-11-10 12:00:31 +0100

    mobile mode

--- js/arena_home.js
+++ js/arena_home.js
@@ -158,13 +158,6 @@ function imageBlock(block) {
     return figure;
 }
 
-// function svgArrow(){
-// 	let arrow = document.createElement('svg')
-// 	var path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-
-
-// }
-
 
 function channelBlock(block, index) {
     let cover = block.contents.find((content) => content.title === "cover");
